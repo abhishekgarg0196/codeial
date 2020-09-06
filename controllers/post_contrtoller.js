d55775f1ebbe18a45d746ae9963062693bcad5ed
abhishek <abhishek.garg0196@gmail.com>
2020-09-06 23:50:41 +0530

    async await conversion in controller

--- controllers/post_contrtoller.js
+++ controllers/post_contrtoller.js
@@ -1,32 +1,63 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 
-module.exports.create = function(req, res){
-    Post.create({
-        content: req.body.content,
-        user: req.user._id
-    }, function(err, post){
-        if(err){
-            console.log(`error in creating a post`);
-            return;
-        }
-        return res.redirect('back');
+module.exports.create = async function(req, res){
+    // Post.create({
+    //     content: req.body.content,
+    //     user: req.user._id
+    // }, function(err, post){
+    //     if(err){
+    //         console.log(`error in creating a post`);
+    //         return;
+    //     }
+    //     return res.redirect('back');
 
-    })
+    // })
+
+    // async + await convention
+    try{
+        await Post.create({
+                content: req.body.content,
+                 user: req.user._id
+             });
+            return res.redirect('back');
+    }catch(err){
+        console.log(`error in creating a post : ${err}`);
+        return;
+    }
 }
 
-module.exports.destroy = function(req, res){
-    Post.findById(req.params.id, function(err, post){
+module.exports.destroy = async function(req, res){
+    // Post.findById(req.params.id, function(err, post){
+    //     // we are comparing the string here .id will auto convert the object id with string
+    //     // .id means converting the obj id into string 
+    //     if(post.user == req.user.id){
+    //         post.remove();
+
+    //         Comment.deleteMany({post:req.params.id}, function(err){
+    //             return res.redirect('back');
+    //         });
+    //     } else {
+    //         return res.redirect('back');
+    //     }
+    // });
+
+    try {
+        let post = await Post.findById(req.params.id);
         // we are comparing the string here .id will auto convert the object id with string
         // .id means converting the obj id into string 
+
         if(post.user == req.user.id){
             post.remove();
-
-            Comment.deleteMany({post:req.params.id}, function(err){
-                return res.redirect('back');
-            });
+            // below code is already async we are await for it to be executed 
+            await Comment.deleteMany({post:req.params.id});
+            return res.redirect('back');
         } else {
             return res.redirect('back');
-        }
-    });
-};
+    }
+    } catch (err) {
+        console.log(`error in creating a post : ${err}`);
+        return;
+    }
+    
+}
