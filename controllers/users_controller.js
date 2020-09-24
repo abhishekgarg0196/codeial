bdeb5603b7c862b01391f7377f4b83744767f275
abhishek <abhishek.garg0196@gmail.com>
2020-09-25 02:16:11 +0530

    Adding flash messages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,88 +1,92 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
+module.exports.profile = function (req, res) {
+  User.findById(req.params.id, function (err, user) {
+    // we can not use the key user as it's already availbale inlocals
+    return res.render("user_profile", {
+      title: "User Profile",
+      profile_user: user,
+    });
+  });
+};
 
-module.exports.profile = function(req, res){
-
-    User.findById(req.params.id, function(err, user){
-        // we can not use the key user as it's already availbale inlocals
-        return res.render('user_profile', {
-            title: 'User Profile',
-            profile_user : user
-        })
-    })
-}
-
-module.exports.update = function(req, res){
-    if(req.user.id == req.params.id) {
-            User.findByIdAndUpdate(req.params.id , {
-                // or req.body 
-                name : req.body.name,
-                email : req.body.email
-            }, function(err, user){
-                    return res.redirect('back');
-            });
-    }else{
-        res.status(401).send('Unauthorized');
-    }
-
-}
-
+module.exports.update = function (req, res) {
+  if (req.user.id == req.params.id) {
+    User.findByIdAndUpdate(
+      req.params.id,
+      {
+        // or req.body
+        name: req.body.name,
+        email: req.body.email,
+      },
+      function (err, user) {
+        return res.redirect("back");
+      }
+    );
+  } else {
+    res.status(401).send("Unauthorized");
+  }
+};
 
 // render the sign up page
-module.exports.signUp = function(req, res){
-    if(req.isAuthenticated()){
-        return res.render("/user/profile");
-    }
-    return res.render('user_sign_up', {
-        title: "Codeial | Sign Up"
-    })
-}
-
+module.exports.signUp = function (req, res) {
+  if (req.isAuthenticated()) {
+    return res.render("/user/profile");
+  }
+  return res.render("user_sign_up", {
+    title: "Codeial | Sign Up",
+  });
+};
 
 // render the sign in page
-module.exports.signIn = function(req, res){
-    if(req.isAuthenticated()){
-        return res.render("/user/profile");
-    }
-    return res.render('user_sign_in', {
-        title: "Codeial | Sign In"
-    })
-}
+module.exports.signIn = function (req, res) {
+  if (req.isAuthenticated()) {
+    return res.render("/user/profile");
+  }
+  return res.render("user_sign_in", {
+    title: "Codeial | Sign In",
+  });
+};
 
 // get the sign up data
-module.exports.create = function(req, res){
-    if (req.body.password != req.body.confirm_password){
-        return res.redirect('back');
+module.exports.create = function (req, res) {
+  if (req.body.password != req.body.confirm_password) {
+    return res.redirect("back");
+  }
+
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log("error in finding user in signing up");
+      return;
     }
 
-    User.findOne({email: req.body.email}, function(err, user){
-        if(err){console.log('error in finding user in signing up'); return}
-
-        if (!user){
-            User.create(req.body, function(err, user){
-                if(err){console.log('error in creating user while signing up'); return}
-
-                return res.redirect('/users/sign-in');
-            })
-        }else{
-            return res.redirect('back');
+    if (!user) {
+      User.create(req.body, function (err, user) {
+        if (err) {
+          console.log("error in creating user while signing up");
+          return;
         }
 
-    });
-}
-
+        return res.redirect("/users/sign-in");
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
+};
 
 // sign in and create a session for the user
-module.exports.createSession = function(req, res){
-    req.flash('success', 'logged in successfully');
-    return res.redirect('/');
-}
+module.exports.createSession = function (req, res) {
+  req.flash("success", "logged in successfully");
+  // here we are redirecting to the home page and thus app m/w will be called and hence the res.locals.flash will be set accordingly
+  return res.redirect("/");
+};
 
-module.exports.destroySession = function(req, res){
-    //This function is given to req by passport
-    req.logout();
-    req.flash('success', 'you have logged out successfully');
+module.exports.destroySession = function (req, res) {
+  //This function is given to req by passport
+  req.logout();
+  req.flash("success", "you have logged out successfully");
 
-    // before redirect to home page we need to log out 
-    return res.redirect('/');
-}
+  // before redirect to home page we need to log out
+  return res.redirect("/");
+};
--- index.js
+++ index.js
@@ -1,70 +1,73 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 8000;
-const expressLayouts = require('express-ejs-layouts');
-const db = require('./config/mongoose');
+const expressLayouts = require("express-ejs-layouts");
+const db = require("./config/mongoose");
 // used for session cookie
-const session = require('express-session');
-const passport = require('passport');
+const session = require("express-session");
+const passport = require("passport");
 //This is just to initialze and notify express app that we are using passport middleware here
-const passportLocal = require('./config/passport-local-strategy');
-const MongoStore = require('connect-mongo')(session);
-const sassMiddleware = require('node-sass-middleware');
-const flash = require('connect-flash');
-const customMware = require('./config/middleware');
+const passportLocal = require("./config/passport-local-strategy");
+const MongoStore = require("connect-mongo")(session);
+const sassMiddleware = require("node-sass-middleware");
+const flash = require("connect-flash");
+const customMware = require("./config/middleware");
 
 // The scss should be pre compiled such that css file are availbale on start of application to the layout/template
-//Put this at a positon just above the server starting 
+//Put this at a positon just above the server starting
 // and also before browser makes a req to fetch the page
-app.use(sassMiddleware({
-    src: './assets/scss',
-    dest : './assets/css',
+app.use(
+  sassMiddleware({
+    src: "./assets/scss",
+    dest: "./assets/css",
     debug: true,
     outputStyle: "extended",
     // Prefix tell the app that by default to look for CSS file in which dir
-    prefix : '/css'  
-}));
+    prefix: "/css",
+  })
+);
 
 app.use(express.urlencoded());
 
 app.use(cookieParser());
 
-app.use(express.static('./assets'));
+app.use(express.static("./assets"));
 
 app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
-app.set('layout extractStyles', true);
-app.set('layout extractScripts', true);
-
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 // set up the view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 //Mongo store is used to store the session cokkies in the db
 // This is to tell what kind of cookies are being configured .. this will be used by passport for serialize and deserialize the user
-app.use(session({
-    name: 'codeial',
+app.use(
+  session({
+    name: "codeial",
     // TODO change the secret before deployment in production mode
-    secret: 'blahsomething',
+    secret: "blahsomething",
     saveUninitialized: false,
     resave: false,
     cookie: {
-        maxAge: (1000 * 60 * 100)
+      maxAge: 1000 * 60 * 100,
     },
-    store : new MongoStore(
-        {
+    store: new MongoStore(
+      {
         mongooseConnection: db,
-        autoRemove:'disabled'
-        },
-        function(err){
-            console.log(err || 'connect mongo setup ok');
-        }
-    )
-}));
+        autoRemove: "disabled",
+      },
+      function (err) {
+        console.log(err || "connect mongo setup ok");
+      }
+    ),
+  })
+);
 
-// this will call de-serialize in passport based on presence of cookies or not 
+// this will call de-serialize in passport based on presence of cookies or not
 app.use(passport.initialize());
 //This is seesion which is created by passport
 app.use(passport.session());
@@ -76,17 +79,15 @@ app.use(flash());
 app.use(customMware.setFlash);
 
 // use express router
-app.use('/', require('./routes'));
+app.use("/", require("./routes"));
 
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error in running the server: ${err}`);
+  }
 
-app.listen(port, function(err){
-    if (err){
-        console.log(`Error in running the server: ${err}`);
-    }
-
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
 
-
-//By default, LocalStrategy expects to find credentials in parameters named username and password. 
+//By default, LocalStrategy expects to find credentials in parameters named username and password.
 //If your site prefers to name these fields differently, options are available to change the defaults.
